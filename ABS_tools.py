05e4055589daa38e34c5568cefe56ee9e3142741
Thomas Manke <manke@mac1104.ie-freiburg.mpg.de>
2022-10-13 18:21:09 +0200

    restructured

---
+++ ABS_tools.py
@@ -0,0 +1,19 @@
+def plot_cm(mat):
+  """
+  Function to plot confusion matrix mat. 
+
+  The matrix could be the output from sklearn: mat = confusion_matrix(y_pred, y_test)
+  """
+
+  classes = np.arange(cm.shape[0])
+  plt.imshow(mat, cmap=plt.cm.Blues)
+  for (j,i),label in np.ndenumerate(mat):
+    plt.text(i,j,np.round(label,2),ha='center',va='center')
+
+  plt.colorbar()
+  plt.title('Confusion Matrix')
+  plt.xlabel('True label')
+  plt.ylabel('Pred label')
+  plt.xticks(classes)
+  plt.yticks(classes)
+  plt.show()
