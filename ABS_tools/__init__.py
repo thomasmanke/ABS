e3d4e9a763294f69b64768f9495c4b2216b03a7a
Thomas Manke <manke@mac1104.ie-freiburg.mpg.de>
2022-10-13 17:00:01 +0200

    moved ABS.py to package directory

---
+++ ABS_tools/__init__.py
@@ -0,0 +1,9 @@
+"""
+The ``ABS_tools`` module provides functionality for the jupyter notebooks of the ABS lecture
+"""
+
+__version__ = '0.0.1'
+__name__ = 'ABS_tools'
+__author__ = 'Thomas Manke - thomasmanke'
+__email__ = ''
+__github__ = 'https://github.com/thomasmanke/ABS'
