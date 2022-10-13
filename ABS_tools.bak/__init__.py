fdb949a337f01ff04e16a29ebb856491279bcfe2
Thomas Manke <manke@mac1104.ie-freiburg.mpg.de>
2022-10-13 18:26:45 +0200

    simplified module

--- ABS_tools.bak/__init__.py
+++
@@ -1,9 +0,0 @@
-"""
-The ``ABS_tools`` package provides functionality for the jupyter notebooks of the ABS lecture
-"""
-
-__version__ = '0.0.1'
-__name__ = 'ABS_tools'
-__author__ = 'Thomas Manke - thomasmanke'
-__email__ = ''
-__github__ = 'https://github.com/thomasmanke/ABS'
